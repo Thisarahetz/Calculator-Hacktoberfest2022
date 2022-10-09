d520d8d69bd3afc3c6e67bba3ffd63fb91b1549e
dinethpiyumantha <dinethpiyumantha@gmail.com>
2022-10-09 16:40:50 +0530

    Calculator style changed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
-// This function clear all the values
+var formular = "";
+
+/**
+ * This function clear all the values
+ * @param
+ * @returns
+ */
 function clearScreen() {
     document.getElementById("result").value = "";
+    document.getElementById("fom").innerHTML = "";
 }
 
-// This function display values
+function displayformular(value) {
+    formular+=value;
+    document.getElementById("fom").innerHTML = formular;
+}
+
+/**
+ *  This function display values
+ * @param value
+ * @returns
+ */
 function display(value) {
+    displayformular(value);
     document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and returns result
+/**
+ * This function evaluates the expression and returns result
+ * @param
+ * @returns
+ */
 function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
 }
 
-// This function remove backspace
+/**
+ * This function remove backspace
+ * @param
+ * @returns
+ */
 function backspace() {
     var back_space = document.getElementById('result');
     var i = back_space.value;
